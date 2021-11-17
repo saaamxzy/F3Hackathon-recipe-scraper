3e11c76cb29cef68c36b1bd79eeb87c05f6e1911
Zhengyuan Xu <zhengyux@amazon.com>
2021-11-17 14:41:18 -0500

    initial commit

---
+++ src/js/getPagesSource.js
@@ -0,0 +1,83 @@
+import JSSoup from 'jssoup'; 
+
+function ingredientObjToStr(ingredientObj) {
+    return "name: " + ingredientObj.name
+     + ", ingredient: " + ingredientObj.ingredient
+     + ", quantity: " 
+     + ingredientObj.quantity 
+     + ", unit: " 
+     + ingredientObj.quantityUnit;
+}
+
+function DOMtoString(document_root) {
+    var html = '',
+        node = document_root.firstChild;
+    while (node) {
+        switch (node.nodeType) {
+        case Node.ELEMENT_NODE:
+            html += node.outerHTML;
+            break;
+        case Node.TEXT_NODE:
+            html += node.nodeValue;
+            break;
+        case Node.CDATA_SECTION_NODE:
+            html += '<![CDATA[' + node.nodeValue + ']]>';
+            break;
+        case Node.COMMENT_NODE:
+            html += '<!--' + node.nodeValue + '-->';
+            break;
+        case Node.DOCUMENT_TYPE_NODE:
+            // (X)HTML documents are identified by public identifiers
+            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+            break;
+        }
+        node = node.nextSibling;
+    }
+
+    
+    var soup = new JSSoup(html);
+    var allUls = soup.findAll("ul");
+    var ingredientsRawArray;
+    for (let i = 0; i < allUls.length; i++) {
+        if (allUls[i].attrs.class == "ingredients-section") {
+            // console.log(allUls[i]);
+            ingredientsRawArray = allUls[i].contents;
+            break;
+        }
+    }
+    // console.log(ingredientsRawArray);
+
+    var resultIngredients = [];
+    var resultStr = "";
+    for (let i = 0; i < ingredientsRawArray.length; i++) {
+        var soupTag = ingredientsRawArray[i];
+        var ingredientAttrs = soupTag.contents[0].contents[0].attrs;
+        var ingredientObj = {
+            name: ingredientAttrs.value,
+            ingredient: ingredientAttrs["data-ingredient"],
+            quantity: ingredientAttrs["data-quantity"],
+            quantityUnit: ingredientAttrs['data-unit']
+        }
+        resultStr += ingredientObjToStr(ingredientObj) + "\n";
+    }
+
+    // for (let ingredient in ingredientsRawArray.contents) {
+    //     console.log(ingredient);
+    // }
+
+    // for (let i = 0; i < ingredientsRawArray.contents.length; i++) {
+    //     var ingredientRaw = ingredientsRawArray
+    //     console.log(ingredientsRawArray.contents);
+    //     // for (innerContent in ingredientsRaw.contents[i]) {
+    //     //     // stuff = innerContent.contents[0].contents[0].attrs;
+    //     //     // console.log(stuff);
+    //     // }
+    // }
+
+    return resultStr;
+}
+
+chrome.runtime.sendMessage({
+    action: "getSource",
+    source: DOMtoString(document)
+});
